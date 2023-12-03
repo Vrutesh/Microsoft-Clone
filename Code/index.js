cef96fd2f4358c25aed951ffc338ca5cc88dd6e5
Vrutesh Mayekar <vruteshpm02@gmail.com>
2023-12-03 14:47:39 +0530

    updated folder structure

--- Code/index.js
+++ Code/index.js
@@ -1,28 +1,28 @@
 // go-to-top-btn
-let calcScrollValue = () => {
-  let gototopbtn = document.querySelector(".back-to-top");
-  let pos = document.documentElement.scrollTop;
+// let calcScrollValue = () => {
+//   let gototopbtn = document.querySelector(".back-to-top");
+//   let pos = document.documentElement.scrollTop;
 
-  if (pos > 300) {
-    gototopbtn.style.display = "flex";
-    gototopbtn.style.bottom = "0px";
-    gototopbtn.style.right = "20px";
+//   if (pos > 300) {
+//     gototopbtn.style.display = "flex";
+//     gototopbtn.style.bottom = "0px";
+//     gototopbtn.style.right = "20px";
 
-    if (pos > 1850) {
-      gototopbtn.style.display = "flex";
-      gototopbtn.style.position = "fixed";
-    }
-  } else {
-    gototopbtn.style.display = "none";
-  }
+//     if (pos > 1850) {
+//       gototopbtn.style.display = "flex";
+//       gototopbtn.style.position = "fixed";
+//     }
+//   } else {
+//     gototopbtn.style.display = "none";
+//   }
 
-  gototopbtn.addEventListener("click", () => {
-    document.documentElement.scrollTop = 0;
-  });
-};
+//   gototopbtn.addEventListener("click", () => {
+//     document.documentElement.scrollTop = 0;
+//   });
+// };
 
-window.onscroll = calcScrollValue;
-window.onload = calcScrollValue;
+// window.onscroll = calcScrollValue;
+// window.onload = calcScrollValue;
 
 //   swiper
 
