66993c1dfe022912e1842d530de7a12254f8efba
Vrutesh Mayekar <vruteshpm02@gmail.com>
2023-12-03 14:38:18 +0530

    completed design and added all sections

---
+++ Code/index.js
@@ -0,0 +1,43 @@
+// go-to-top-btn
+let calcScrollValue = () => {
+  let gototopbtn = document.querySelector(".back-to-top");
+  let pos = document.documentElement.scrollTop;
+
+  if (pos > 300) {
+    gototopbtn.style.display = "flex";
+    gototopbtn.style.bottom = "0px";
+    gototopbtn.style.right = "20px";
+
+    if (pos > 1850) {
+      gototopbtn.style.display = "flex";
+      gototopbtn.style.position = "fixed";
+    }
+  } else {
+    gototopbtn.style.display = "none";
+  }
+
+  gototopbtn.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
+
+//   swiper
+
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
